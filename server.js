51d39c3b47c9cc8e93203ff0db7c2af80e441c6c
ramynbl <r_nebili@hetic.eu>
2025-12-10 22:26:03 +0100

    Update entry point to server.js and configure PORT from environment variable

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 
 if (!process.env.GEMINI_API_KEY) {
